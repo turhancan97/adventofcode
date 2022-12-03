09a49c29a26989c5e793e6d154be432113b72de5
Turhan Can Kargın <turhancan.kargin@gmail.com>
2022-12-03 17:23:11 +0100

    improvement

--- Day 3/3a.py
+++ Day 3/3a.py
@@ -11,23 +11,23 @@ with open('rucksacks.txt', 'r', encoding="utf-8") as f: # Open the file
 
         # Create dictionaries that map each character to the number of times
         # it appears in each compartment
-        counts1, counts2 = {}, {}
-        for ch in comp1:
+        counts1, counts2 = {}, {} # Initialize the dictionaries
+        for ch in comp1: # Loop through each character in the first compartment
             counts1[ch] = counts1.get(ch, 0) + 1
-        for ch in comp2:
+        for ch in comp2: # Loop through each character in the second compartment
             counts2[ch] = counts2.get(ch, 0) + 1
 
         # Loop through the keys in the first dictionary
-        for ch in counts1.keys():
+        for ch in counts1.keys(): # Loop through each character in the first compartment
             # If the character appears in both compartments, add its priority
             # to the sum of priorities
-            if ch in counts2:
+            if ch in counts2: # If the character appears in both compartments
                 # Lowercase item types a through z have priorities 1 through 26.
                 # Uppercase item types A through Z have priorities 27 through 52.
                 if ch.islower():
-                    sum_of_priorities += ord(ch) - 96
+                    sum_of_priorities += ord(ch) - 96 # Add the priority 
                 else:
-                    sum_of_priorities += ord(ch) - 38
+                    sum_of_priorities += ord(ch) - 38 # Add the priority
 
     # Print the sum of priorities
     print(sum_of_priorities)
