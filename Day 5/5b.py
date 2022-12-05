932e6855da6b40807ce46e7274bd019caa10f50b
Turhan Can Kargın <turhancan.kargin@gmail.com>
2022-12-05 12:17:41 +0100

    Day 5 done

---
+++ Day 5/5b.py
@@ -0,0 +1,33 @@
+N=9
+
+# Opening the file, reading the lines of the file, removing the newline character from each line, and
+# creating a list of lists.
+with open('stack.txt', 'r', encoding="utf-8") as f: # Open the file
+    stack = f.readlines() # Read the lines of the file
+    stack = [line.strip() for line in stack] # Remove the newline character from each line
+    S = [[] for _ in range(N)]
+    
+    # To save each colums of a stack to a list
+    for i in range(N-1):
+        line = stack[i]
+        crates = line[1::4]
+        for s in range(len(crates)):
+            if crates[s] != " ":
+                S[s].append(crates[s])
+
+    # Reverse all stacks
+    stacks = [stack[::-1] for stack in S]
+
+    # Move things around
+    for line in stack[N+1:]:
+        tokens = line.split(" ")
+        n, src, dst = map(int, [tokens[1], tokens[3], tokens[5]])
+        src -= 1
+        dst -= 1
+
+        stacks[dst].extend(stacks[src][-n:])
+        stacks[src] = stacks[src][:-n]
+
+
+    tops = [stack[-1] for stack in stacks]
+    print("".join(tops))
